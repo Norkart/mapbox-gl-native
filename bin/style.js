355189f10093104246d0edd72042936dc308fca5
Robert Nordan <rpvn@robpvn.net>
2014-06-18 12:25:41 +0200

    Commented on some style properties

--- bin/style.js
+++ bin/style.js
@@ -1,29 +1,29 @@
-"use strict";
+"use strict"; //Min tolkning av stilarket. Finnes en spec på github.com/mapbox/mapbox-gl-style-spec, men er veldig uferdig.
 
 module.exports = {
-    "buckets": {
+    "buckets": { //Buckets angir objekttyper som skal males på samme måte, virker det som.
         "bane": {
-            "source": "outdoors", //Egentlig robpvn.12o4x977, men navnet den får ved innlesing er hardkodet
+            "source": "outdoors", //Egentlig robpvn.12o4x977, men navnet den får ved innlesing er foreløpig hardkodet
             "layer": "bane",
             "field": "objtype",
             "value": "Bane",
             "type": "line"
         }
     },
-    "structure": [
+    "structure": [ //Structure angir hvilken objekttype (bucket) som assosieres med hvilken tegneregel.
         {
             "name": "bane_Bane",
             "bucket": "bane"
         }
     ],
-    "constants": {
+    "constants": { //Er konstantverdier som kan refereres fra tegneregler
         "land": "rgb(244,239,225)",
         "water": "#cdd"
     },
-    "classes": [
+    "classes": [ //Classes er samlinger av tegneregler, det vil si tema/tegneregelsett det kan skiftes mellom
         {
             "name": "default",
-            "layers": {
+            "layers": {	//Tegneregel
                 "bane_Bane": {
                     "color": "#000000",
                     "width": 6.8
@@ -31,5 +31,5 @@ module.exports = {
             }
         }
     ],
-    "sprite": "/img/maki-sprite"
+    "sprite": "/img/maki-sprite" //Tror dette referer til spriteskjemaet som skal brukes, hvorav sprites refereres med navn i tegnereglene, men usikker på hvordan dette forholder seg til spriteURL i constants.cpp
 };
