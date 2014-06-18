0b126b369d5b796fee1718769ac55d6f80784a54
Robert Nordan <rpvn@robpvn.net>
2014-06-18 12:10:24 +0200

    Experimental adding of a different tile provider & styles

--- config/constants.cpp
+++ config/constants.cpp
@@ -1,7 +1,7 @@
 #include <llmr/util/constants.hpp>
 
 const char *llmr::kSpriteURL = "http://mapbox-kkaefer.s3.amazonaws.com/static/outdoors-gl/sprite";
-const char *llmr::kVectorTileURL = "http://a.tiles.mapbox.com/v3/mapbox.mapbox-terrain-v1,mapbox.mapbox-streets-v5/%d/%d/%d.vector.pbf";
+const char *llmr::kVectorTileURL = "http://a.tiles.mapbox.com/v3/robpvn.12o4x977/%d/%d/%d.vector.pbf";
 const char *llmr::kGlyphURL = "http://mapbox.s3.amazonaws.com/gl-glyphs-256/%s/%d-%d.pbf";
 
 const float llmr::util::tileSize = 512.0f;
@@ -18,10 +18,10 @@ const bool llmr::debug::missingFontFaceWarning = true;
 const bool llmr::debug::glyphWarning = true;
 const bool llmr::debug::shapingWarning = true;
 #else
-const bool llmr::debug::tileParseWarnings = false;
-const bool llmr::debug::styleParseWarnings = false;
-const bool llmr::debug::spriteWarnings = false;
-const bool llmr::debug::renderWarnings = false;
+const bool llmr::debug::tileParseWarnings = true;
+const bool llmr::debug::styleParseWarnings = true;
+const bool llmr::debug::spriteWarnings = true;
+const bool llmr::debug::renderWarnings = true;
 const bool llmr::debug::renderTree = false;
 const bool llmr::debug::labelTextMissingWarning = false;
 const bool llmr::debug::missingFontStackWarning = false;
